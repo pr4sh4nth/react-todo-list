20e4c4f1063f47cfbe3a42429ea2c13711f841ea
prashanth <prash488@gmail.com>
2021-04-16 12:02:57 +0800

    Change up App.js layout, add material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+    <div > 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="App-title"> Todo App</h1>
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
+   
+    
+    
     </div>
-  );
+
+    )
+  }
 }
+
 
 export default App;
